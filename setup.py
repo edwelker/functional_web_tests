1952bca2cac1d37aa1bff16ffa6844d3c2f1ebc7
Eddie Welker <eddie.welker@gmail.com>
2013-03-04 11:56:59 -0500

    moved things around

---
+++ setup.py
@@ -0,0 +1,19 @@
+!#/usr/bin/env python
+# vim:fileencoding=utf-8
+from distutils.core import setup
+
+requirements = [item for item in open('requirements.txt').read().splitlines()]
+
+setup(
+    name = "",
+    version = "0.1.0",
+    url = "",
+    author = "Edward Welker",
+    author_email = "eddie.welker@gmail.com",
+    description = "A foundational selenium based web testing framework",
+    license = "Public Domain",
+    requirements = requirements,
+
+
+
+)
